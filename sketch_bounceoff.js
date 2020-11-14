ad5ad499d0b0c3c0d7d76d997ef8df1e986d474e
loma191127 <69203135+loma191127@users.noreply.github.com>
2020-11-14 12:57:30 +0530

    all files of boilerplate

---
+++ sketch_bounceoff.js
@@ -0,0 +1,31 @@
+var car,wall;
+var mass,speed;
+var deform;
+function setup(){
+  createCanvas(800,800);
+car=createSprite(100,400,50,10);
+wall=createSprite(750,400,100,800);
+speed=random(5,10);
+mass=random(2,10);
+car.velocityX=speed;
+
+}
+function draw(){
+  background(0);
+  if(wall.x-car.x < car.width/2+wall.width/2){
+    car.velocityX=0;
+    deform=(0.5*mass*speed*speed);
+    if(deform>180){
+      car.shapeColor=rgb(100,0,0)
+    }
+    if(deform>100 && deform<180){
+      car.shapeColor=rgb(100,100,0);
+    }
+    if(deform<100){
+      car.shapeColor=rgb(0,100,0)
+    }
+
+  }
+  drawSprites();
+}
+
